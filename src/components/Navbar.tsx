64b331aa6c9a42acbb038837d8e787bfd6aa2ac2
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-02 18:21:07 -0400

    Added some finishing touches to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,9 @@ const Navbar = () => {
   const user = false
 
   return (
-    <div className="h-12 text-black uppercase flex justify-between items-center p-4 border-b-2 border-b-black">
-      <div className="hidden md:flex gap-4">
+    <div className=" lg:px-20 xl:pg-40 h-12 text-black uppercase flex justify-between items-center p-4 border-b-2 border-b-black md:h-24">
+
+      <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Home</Link>
         <Link href="/menu">Keyboards</Link>
         <Link href="/">Contact</Link>
@@ -17,7 +18,7 @@ const Navbar = () => {
 
       {/* { This is for the logo } */}
 
-      <div className="text-xl">
+      <div className="text-xl flex-1 md:text-center md:font-bold">
         <Link href="/">MechMarket</Link>
       </div>
 
@@ -27,7 +28,7 @@ const Navbar = () => {
       </div>
 
 
-      <div className="hidden md:flex gap-4 items-center">
+      <div className="hidden md:flex gap-4 items-center flex-1 justify-end">
         {!user ? (<Link href="/login">Login</Link>) : (<Link href="/orders">Orders</Link>)}
         <CartIcon></CartIcon>
       </div>
