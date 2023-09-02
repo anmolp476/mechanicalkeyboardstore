399bbb85fe286590c67b9948b3ab80f75a25c4e9
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-02 17:56:47 -0400

    I finished the navbar, which includes a working menu button with menu that is mobile responsive, also added some touches to the notification bar at the very top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
+import CartIcon from "./CartIcon"
 
 const Navbar = () => {
+
+  const user = false
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="h-12 text-black uppercase flex justify-between items-center p-4 border-b-2 border-b-black">
+      <div className="hidden md:flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/menu">Keyboards</Link>
+        <Link href="/">Contact</Link>
+      </div>
+
+      {/* { This is for the logo } */}
+
+      <div className="text-xl">
+        <Link href="/">MechMarket</Link>
+      </div>
+
+      {/* { This is for the mobile-version of the menu } */}
+      <div className="md:hidden">
+        <Menu></Menu>
+      </div>
+
+
+      <div className="hidden md:flex gap-4 items-center">
+        {!user ? (<Link href="/login">Login</Link>) : (<Link href="/orders">Orders</Link>)}
+        <CartIcon></CartIcon>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div className="cursor-pointer h-12 bg-slate-950 text-white text-center text-sm px-4 flex justify-center items-center md:text-base">
+      All Mechanical Keyboards 50% Off For The Next 24 Hours, Order Now!
+    </div>
   )
 }
 
