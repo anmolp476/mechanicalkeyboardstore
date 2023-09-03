5271437c946edb949c6c5a6220f26989bbb8c897
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-02 20:33:30 -0400

    Finished the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="h-12 md:h-24 lg:p-20 xl:p-40 p-4 md:px-24 text-black flex justify-between items-center">
+      <Link href="/" className="font-bold text-l">
+        MechMarket
+      </Link>
+      <p>Made with ❤️ By Anmol</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
