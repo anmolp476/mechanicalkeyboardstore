7155f2c4693fee0e85e991a187639d1c49be8e43
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-06 13:26:43 -0400

    Completed the cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,25 +1,82 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-black lg:flex-row">
+      {/* This div will contain the products that are in the cart */}
+      <div className="h-1/2 p-4 pt-24 sm:pt-0 flex flex-col justify-center overflow-y-scroll lg:h-full lg:w-3/4 2xl:w-1/2 lg:px-20 xl:px-40">
+        {/* This will contain a single item */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/keyboard_1.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-lg font-bold text-center">
+              Leopold FC900R
+            </h1>
+            <span className="text-xs flex items-center justify-center">
+              Cherry MX Brown
+            </span>
+          </div>
+          <h2 className="pr-5">$149.99</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
 
-export default Cart
+        {/* This will contain a single item */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/keyboard_1.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-lg font-bold text-center">
+              Leopold FC900R
+            </h1>
+            <span className="text-xs flex items-center justify-center">
+              Cherry MX Brown
+            </span>
+          </div>
+          <h2 className="pr-5">$149.99</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
 
+        {/* This will contain a single item */}
+        <div className="flex items-center justify-between mb-4">
+          <Image src="/keyboard_1.jpg" alt="" width={100} height={100} />
+          <div className="">
+            <h1 className="uppercase text-lg font-bold text-center">
+              Leopold FC900R
+            </h1>
+            <span className="text-xs flex items-center justify-center">
+              Cherry MX Brown
+            </span>
+          </div>
+          <h2 className="pr-5">$149.99</h2>
+          <span className="cursor-pointer">X</span>
+        </div>
+      </div>
 
+      {/* This div will contain the payment*/}
+      <div className="flex flex-col gap-4 justify-center h-1/2 p-4 pt-32 sm:pt-5 lg:h-full lg:w-1/2 2xl:w-1/2 lg:px-10 xl:px-40">
+        <div className="flex justify-between">
+          <span className="">Subtotal (3 items)</span>
+          <span className="">$159.99</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="">Service Cost</span>
+          <span className="">$0.00</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="">Delivery Cost</span>
+          <span className="text-blue-500">FREE!</span>
+        </div>
+        <hr className="my-2" />
+        <div className="flex justify-between">
+          <span className="">Total Cost</span>
+          <span className="font-bold">$159.99</span>
+        </div>
+        <button className="bg-black text-white p-3 rounded-md w-1/2 self-end">
+          Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Cart;
