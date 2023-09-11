dfbce8aca58e98bd89594c6cdbc57429e71c60b4
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-11 16:26:52 -0400

    Now the number you see beside the cart icon on the top-right will match the number of products in the Cart

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import Link from "next/link"
-import { FiShoppingCart } from "react-icons/fi"
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { FiShoppingCart } from "react-icons/fi";
+import { useCartStore } from "@/utils/store";
 
 const CartIcon = () => {
+
+  const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+
   return (
     <Link href="/cart" className="flex items-center gap-4">
-        <div className="relative w-8 h-8 md:w-5 md:h-5">
-            <FiShoppingCart style={{width: "100%", height:"100%"}}></FiShoppingCart>
-        </div>
-        <span>Cart (3)</span>
+      <div className="relative w-8 h-8 md:w-5 md:h-5">
+        <FiShoppingCart
+          style={{ width: "100%", height: "100%" }}
+        ></FiShoppingCart>
+      </div>
+      <span>Cart ({totalItems})</span>
     </Link>
-  )
-}
+  );
+};
 
-export default CartIcon
+export default CartIcon;
