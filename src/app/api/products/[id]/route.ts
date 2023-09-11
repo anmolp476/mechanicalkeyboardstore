b9ddcdf23786335be5cc10f21f5f0b5181195aca
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-10 22:03:23 -0400

    Created the exact same [id] folder and route.ts as in the orders/ folder, but this time it will be a GET request

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+
+  try {
+
+    const singleProd = await prisma.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return new NextResponse(
+      JSON.stringify(JSON.stringify(singleProd)),
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong, orders/[id]/route.ts" }),
+      { status: 500 }
+    );
+  }
+};
