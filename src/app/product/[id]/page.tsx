96b58bb492b231052e9587f3d344b8d3d245c047
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-10 22:11:29 -0400

    We're now going to use the products in the database instead of the ones in the data.ts file, to do this, the getData() function will be called again as a GET method to the products API, also the Type created in the Price.tsx file mistakenly has an id field set to an integer

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,16 +1,38 @@
 import Price from "@/components/Price";
 import { singleProduct } from "@/data";
+import { ProdType } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const ProductPage = () => {
+
+
+const getData = async (id: String) => {
+  const res = await fetch(
+    `http://localhost:3000/api/products/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("getData Failed");
+  }
+
+  return res.json();
+};
+
+
+const ProductPage = async({params}:{params:{id:string}}) => {
+
+  const singleProd : ProdType = await getData(params.id)
+
   return (
     <div className="md:gap-8 lg:px-20 xl:px-40 p-4 h-screen gap-4 flex flex-col md:items-center justify-around text-black md:flex-row ">
       {/* Another img div */}
-      {singleProduct.image && (
+      {singleProd.image && (
         <div className="relative w-full h-1/6 sm:h-1/2">
           <Image
-            src={singleProduct.image}
+            src={singleProd.image}
             alt=""
             className="object-contain"
             fill
@@ -19,12 +41,12 @@ const ProductPage = () => {
       )}
       {/* Another text div */}
       <div className="h-1/2 flex flex-col gap-4 mb-16 md:mb-0 ml-0 sm:ml-5 md:justify-center md:gap-4 xl:gap-6">
-        <h1 className="text-2xl font-bold uppercase">{singleProduct.title}</h1>
+        <h1 className="text-2xl font-bold uppercase">{singleProd.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price
-          id={singleProduct.id}
-          price={singleProduct.price}
-          options={singleProduct.options}
+          id={singleProd.id}
+          price={singleProd.price}
+          options={singleProd.options}
         />
       </div>
     </div>
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 
 type Props = {
   price: number;
-  id: number;
+  id: string;
   options?: { title: string; additionalPrice: number }[];
 };
 
@@ -13,23 +13,21 @@ const Price = ({ price, id, options }: Props) => {
   const [quantity, setQuantity] = useState(1);
   const [selectedButt, setButt] = useState(0);
 
-
   useEffect(() => {
-
     // console.log("1st", quantity * (options ? price + options[selectedButt].additionalPrice : price))
     // console.log("2nd", options ? price + options[selectedButt].additionalPrice:"Does not exist")
     // console.log("3rd", price)
     // console.log("4rd", options ? options[selectedButt].additionalPrice : "does not exist")
-    
-    setTotal(quantity * (options ? price + options[selectedButt].additionalPrice : price))
-  
-  }, [quantity, selectedButt, options, price])
-  
-  
+
+    setTotal(
+      quantity *
+        (options ? price + options[selectedButt].additionalPrice : price)
+    );
+  }, [quantity, selectedButt, options, price]);
 
   return (
     <div className="flex flex-col gap-3">
-      <h2 className="text-xl font-bold">${total.toFixed(2)}</h2>
+      <h2 className="text-xl font-bold">${total}</h2>
       {/* This is the div for the options */}
 
       <div className="flex gap-4">
@@ -39,9 +37,9 @@ const Price = ({ price, id, options }: Props) => {
             className="p-2 ring-1 ring-black rounded-md hover:"
             style={{
               background: selectedButt === index ? "black" : "white",
-              color: selectedButt === index ? "white" : "black"
+              color: selectedButt === index ? "white" : "black",
             }}
-            onClick={()=>setButt(index)}
+            onClick={() => setButt(index)}
           >
             {option.title}
           </button>
@@ -55,9 +53,17 @@ const Price = ({ price, id, options }: Props) => {
         <div className="flex justify-between w-full p-2 ring-1 ring-black">
           <span>Quantity...</span>
           <div className="flex gap-4 items-center">
-            <button onClick={() => setQuantity((curr) => (curr > 1 ? curr-1 : 1))}>{"<"}</button>
+            <button
+              onClick={() => setQuantity((curr) => (curr > 1 ? curr - 1 : 1))}
+            >
+              {"<"}
+            </button>
             <span>{quantity}</span>
-            <button onClick={() => setQuantity((curr) => (curr > 99 ? 99 : curr + 1))}>{">"}</button>
+            <button
+              onClick={() => setQuantity((curr) => (curr > 99 ? 99 : curr + 1))}
+            >
+              {">"}
+            </button>
           </div>
         </div>
 
