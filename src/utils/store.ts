16ef6e9e6bbefe0e8f1873f2ad5b4b8f9eabc60e
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-11 16:14:02 -0400

    This .ts file is where we use Zustand, here I use Zustand's own create function that creates a hook for us. This hook has the ability to apply 2 actions, with the use of the 3 initial states

---
+++ src/utils/store.ts
@@ -0,0 +1,37 @@
+import { ActionType, CartType } from "@/types/types";
+import { create } from "zustand";
+
+const initialStateOfProd = {
+    products: [],
+    totalItems: 0,
+    totalPrice: 0,
+}
+
+const useCartStore = create<CartType & ActionType>((set, get) => ({
+    products: initialStateOfProd.products,
+    totalItems: initialStateOfProd.totalItems,
+    totalPrice: initialStateOfProd.totalPrice,
+
+    addToCart(item) {
+        set((state)=>({
+            products:[...state.products, item],
+            totalItems:state.totalItems+item.quantity,
+            totalPrice: state.totalPrice+item.price
+            
+        }))
+    },
+
+    removeFromCart(item) {
+        set((state)=>({
+            products:state.products.filter(product=>product.id!==item.id),
+            totalItems:state.totalItems-item.quantity,
+            totalPrice: state.totalPrice-item.price
+        }))
+    },
+
+}));
+
+
+
+
+
