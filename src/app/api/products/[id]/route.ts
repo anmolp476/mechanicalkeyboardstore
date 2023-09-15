b7fb4fd91f71bb04a0bcdd7be201e0326c5dc7a9
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-14 21:35:44 -0400

    Created a DELETE request for the product with a specific ID, which is what the handleDelete function will use in DeleteButtonAdmin.tsx

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -23,4 +24,35 @@ export const GET = async (
       { status: 500 }
     );
   }
+};
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const session = await getAuthSession()
+
+  if(session?.user.isAdmin){
+    try {
+      await prisma.product.delete({
+        where: {
+          id: id,
+      },
+    });
+    
+    return new NextResponse(JSON.stringify("Product deleted!"), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong, orders/[id]/route.ts" }),
+      { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: "Not autheticated!" }),
+    { status: 500 }
+  );
 };
