ffef740af6212a52d7f7fabf6872c288976805b5
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-14 23:22:31 -0400

    Used the client's secret key to create a POST request, and also created a new CheckoutForm component

--- src/app/payment/[id]/page.tsx
+++ src/app/payment/[id]/page.tsx
@@ -1,35 +1,53 @@
-"use client"
+"use client";
 
+import CheckoutForm from "@/components/CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { StripeElementsOptions } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React, { useEffect, useState } from "react";
 
-import React, { useEffect } from 'react'
+const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY!);
 
-const Payment = ({params} : {params:{id:string}}) => {
+const Payment = ({ params }: { params: { id: string } }) => {
+  const id = params.id;
 
-  const id = params.id
-
-  
-
+  const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     const req = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/create-intent/${id}`, {
-          method: "POST",
-
-        })
-        const data = await response.json()
+        const response = await fetch(
+          `http://localhost:3000/api/create-intent/${id}`,
+          {
+            method: "POST",
+          }
+        );
+        const data = await response.json();
+        setClientSecret(data.clientSecret);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
 
-    req()
-  }, [id])
-  
+    req();
+  }, [id]);
+
+  const options:StripeElementsOptions={
+    clientSecret,
+    appearance:{
+      theme:"night"
+    }
+  }
 
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm/>
+        </Elements>
+      )}
+    </div>
+  );
+};
 
-export default Payment  
+export default Payment;
