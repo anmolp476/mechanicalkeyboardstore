65278999e5f0ea05b16f32ffa5602f1ed1b83cd4
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-14 21:09:50 -0400

    Created a client-side button only seen by the admin, the UI is done, but just need to work on the backend so it actually does show only for the admin User

---
+++ src/components/DeleteButtonAdmin.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const DeleteButtonAdmin = () => {
+  return (
+    <button className="bg-black p-2 text-white rounded-full absolute top-4 right-4"></button>
+  )
+}
+
+export default DeleteButtonAdmin
