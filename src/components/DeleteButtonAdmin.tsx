6f69a4ff7d17093cc8742dc4caee85a4780fb9cd
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-14 21:24:35 -0400

    Now the button should only show for the Admin, and this was accomplished by using the useSession() hook, which gave the current user's session information

--- src/components/DeleteButtonAdmin.tsx
+++ src/components/DeleteButtonAdmin.tsx
@@ -1,11 +1,26 @@
 "use client"
 
 import React from 'react'
+import { useSession } from "next-auth/react"
+import { useRouter } from 'next/router'
 
 const DeleteButtonAdmin = () => {
-  return (
-    <button className="bg-black p-2 text-white rounded-full absolute top-4 right-4">Delete</button>
-  )
+
+
+    const{data:session, status} = useSession()
+    const router = useRouter()
+
+    if(status=="loading"){
+        return(<p>Loading...</p>)
+    }
+
+    if (status == "unauthenticated" || !session?.user.isAdmin) {
+      return null
+    }
+
+    return (
+        <button className="bg-black p-2 text-white rounded-full absolute top-4 right-4">Delete</button>
+    )
 }
 
 export default DeleteButtonAdmin
