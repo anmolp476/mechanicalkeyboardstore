b9329f07b4931b5e1485c26d5e3f559c4fc621df
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-16 20:21:05 -0400

    Completed the success page, added a PUT request handler to the confirm folder of the api folder, while providing it the payment Intent

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Success = () => {
-  return (
-    <div>Success</div>
-  )
-}
+  const params = useSearchParams();
+  const paymentIntent = params.get("payment_intent");
+  const router = useRouter();
 
-export default Success
+  useEffect(() => {
+    const request = async () => {
+      try {
+        await fetch(`http://localhost:3000/api/confirm${paymentIntent}`, {
+          method: "PUT",
+        });
+        router.push("/orders")
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    request()
+  }, [paymentIntent, router]);
+
+  return <div>Payment successful, please do not close the page!!</div>;
+};
+
+export default Success;
