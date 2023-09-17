5bf67c556a247f5c7ca512bc1e6be5e460fff1b2
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-16 21:46:06 -0400

    The production error is askin for keys so I'm just goin to put in random keys

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { getRandomValues } from "crypto";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -36,17 +37,18 @@ const Slider = () => {
   }, []);
 
   return (
-    <div className="flex flex-col lg:flex-row h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
+    <div key={"fddsiisaw"} className="flex flex-col lg:flex-row h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
       {/* Text container */}
-      <div className="h-1/2 flex justify-center items-center flex-col gap-8 text-black font-bold lg:h-full lg:w-1/2 lg:p-10">
-        <h1 className="text-2xl text-center uppercase p-4 md:text-4xl xl:text-7xl">
+      <div key={"dfjaoii"} className="h-1/2 flex justify-center items-center flex-col gap-8 text-black font-bold lg:h-full lg:w-1/2 lg:p-10">
+        <h1 key={"ahdsoifa;s"} className="text-2xl text-center uppercase p-4 md:text-4xl xl:text-7xl">
           {sliderItems[image].title}
         </h1>
-        <button className="bg-black text-white py-4 px-8">Order Now!</button>
+        <button key={"aifj;aoiewjf"} className="bg-black text-white py-4 px-8">Order Now!</button>
       </div>
       {/* Image container */}
       <div className="h-1/2 relative w-full lg:w-1/2 lg:h-full">
         <Image
+          key={"haweioa;eiowfj"}
           src={sliderItems[image].image}
           alt=""
           fill
