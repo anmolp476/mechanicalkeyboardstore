b3bef5856282b98de05600fb9685736c56c2ea69
anmolp476 <99687931+anmolp476@users.noreply.github.com>
2023-09-18 11:24:02 -0400

    Forgot to change localhost to 127.0.0.1 for the success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -11,7 +11,7 @@ const Success = () => {
   useEffect(() => {
     const request = async () => {
       try {
-        await fetch(`http://localhost:3000/api/confirm${paymentIntent}`, {
+        await fetch(`http://127.0.0.1:3000/api/confirm${paymentIntent}`, {
           method: "PUT",
         });
         router.push("/orders")
